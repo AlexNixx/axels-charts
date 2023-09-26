fe49397d29a55eba29a3a16fe419d0cdc77b9dc5
AlexNixx <94720997+AlexNixx@users.noreply.github.com>
2023-09-26 09:21:06 +0300

    update: fix mistake

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import { Paper } from '@mui/material';
 
 import { CoinTable } from './components/CoinTable';
 
-export const App = () => {
-    return (
-        <Paper>
-            <CoinTable />
-        </Paper>
-    );
-};
+export const App = () => (
+    <Paper>
+        <CoinTable />
+    </Paper>
+);
